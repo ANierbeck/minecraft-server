148d710da9ef3591261fa8daba8ba9e188d3eb29
anierbeck <achim.nierbeck@codecentric.de>
2021-08-31 19:24:17 +0200

    disabled plugins again

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-//import '@aws-amplify/ui-vue';
+import '@aws-amplify/ui-vue';
 
-import Amplify, * as AmplifyModules from 'aws-amplify';
-import { AmplifyPlugin } from '@aws-amplify/ui-vue';
+//import Amplify, * as AmplifyModules from 'aws-amplify';
+import Amplify from 'aws-amplify';
+//import { AmplifyPlugin } from '@aws-amplify/ui-vue';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
-//Vue.use(AmplifyPlugin, AmplifyModules);
 
 Vue.config.productionTip = false
-Vue.use(AmplifyPlugin, AmplifyModules);
+//Vue.use(AmplifyPlugin, AmplifyModules);
 
 new Vue({
   render: h => h(App),
